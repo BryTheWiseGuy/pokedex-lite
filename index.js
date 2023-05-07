11eaba9a27e74ec3547ea43c07c7e4b59a0ceac3
Brian Ashman <brian.ashman@comcast.net>
2023-05-06 21:49:25 -0700

    Adds significant overhauls to Javascript code as well as CSS styling. Fixes minor typos in JSON. Fixes errors in HTML formatting.

--- index.js
+++ index.js
@@ -3,59 +3,155 @@ document.addEventListener('DOMContentLoaded', (e) => {
     createPokemonSelection();
     let selectedCard = document.querySelector('#searchButton')
     selectedCard.addEventListener('click', (e) => {
-        e.preventDefault();
-        fetch('http://localhost:3000/pokemon')
+      e.preventDefault();
+      fetch('http://localhost:3000/pokemon')
         .then(res => res.json())
         .then(data => {
-            renderCard(data)
-            createTypeButtons(data);
+          renderCard(data);
+          renderStats(data);
         })
         .catch(error => console.error(error));
     })
-})
-
-function createPokemonSelection() {
-    fetch('http://localhost:3000/pokemon')
-    .then(res => res.json())
-    .then(data => {
-        data.forEach((pokemon)=> {
+  
+    function createPokemonSelection() {
+      fetch('http://localhost:3000/pokemon')
+        .then(res => res.json())
+        .then(data => {
+          data.forEach((pokemon) => {
             let pokemonSelection = document.querySelector('#pokemonSelect');
             let option = document.createElement('option');
             option.innerText = pokemon.name;
             pokemonSelection.appendChild(option);
+          })
         })
-    })
-}
+    }
 
-function createTypeButtons(data) {
-    let selectedPokemon = document.querySelector('#pokemonSelect')
-    let pokemonTypeContainer = document.querySelector('#pokemonCard')
-    let types = ['Grass', 'Fire', 'Water', 'Electric', 'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'];
-    let selectedPokemonType = data.find(pokemon => pokemon.name === selectedPokemon.value).type;
-    for (let i = 0; i < types.length; i++) {
-        if (selectedPokemonType.includes(types[i])) {
+    function createTypeButtons(data) {
+        let selectedPokemon = document.querySelector('#pokemonSelect');
+        let pokemonTypeContainer = document.querySelector('#pokemonType');
+        let types = ['Grass', 'Fire', 'Water', 'Electric', 'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'];
+        let selectedPokemonType = data.find(pokemon => pokemon.name === selectedPokemon.value).type;
+        for (let i = 0; i < types.length; i++) {
+          if (selectedPokemonType.includes(types[i])) {
             const typeButton = document.createElement('button');
             typeButton.innerText = types[i];
             typeButton.classList.add(types[i]);
             pokemonTypeContainer.appendChild(typeButton);
+          }
+        }
+      }
+  
+    function renderCard(data) {
+        resetCard();
+        resetStats();
+        let pokemonCard = document.querySelector('#pokemonCard');
+        let pokemonTypeContainer = document.createElement('div');
+        let pokemonImage = document.createElement('img');
+        let pokemonName = document.createElement('h3');
+        pokemonTypeContainer.id = 'pokemonType';
+        pokemonImage.src = data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).image;
+        pokemonName.innerText = data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).name;
+        pokemonCard.appendChild(pokemonImage);
+        pokemonCard.appendChild(pokemonName);
+        pokemonCard.appendChild(pokemonTypeContainer);
+        createTypeButtons(data);
+    }
+
+    function renderStats(data) {
+        let pokemonStats = document.querySelector('#pokemonStats');
+        let pokemonHeight = document.createElement('h4');
+        let pokemonWeight = document.createElement('h4');
+        let pokemonCategory = document.createElement('h4');
+        let pokemonWeakness = document.createElement('h4');
+        let pokemonAbility = document.createElement('h4');
+        let pokemonAbilityButtons = document.createElement('div');
+        let pokemonWeaknessButtons = document.createElement('div');
+        pokemonAbilityButtons.id = 'pokemonAbilities';
+        pokemonWeaknessButtons.id = 'pokemonWeaknessButtons';
+        pokemonAbility.innerText = 'Abilities:';
+        pokemonWeakness.innerText = 'Weaknesses:';
+        pokemonHeight.innerText = `Height: ${data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).height}`;
+        pokemonWeight.innerText = `Weight: ${data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).weight}`;
+        pokemonCategory.innerText = `Category: ${data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).category}`;
+        pokemonStats.appendChild(pokemonHeight);
+        pokemonStats.appendChild(pokemonWeight);
+        pokemonStats.appendChild(pokemonCategory);
+        pokemonStats.appendChild(pokemonWeakness);
+        pokemonStats.appendChild(pokemonWeaknessButtons);
+        pokemonStats.appendChild(pokemonAbility);
+        pokemonStats.appendChild(pokemonAbilityButtons);
+        renderWeakness(data);
+        renderAbility(data);
+        renderDescription(data);
+    }
+
+    function renderAbility(data) {
+        let selectedPokemon = document.querySelector('#pokemonSelect');
+        let pokemonAbilityContainer = document.querySelector('#pokemonAbilities');
+        let abilities = [...new Set(data.map(pokemon => pokemon.abilities).flat())];
+        let selectedPokemonAbilities = data.find(pokemon => pokemon.name === selectedPokemon.value).abilities;
+        let abilityInfoBox = document.createElement('div');
+        let overlay = document.createElement('div');
+        let abilityInfo = document.createElement('p');
+        overlay.id = 'overlay';
+        abilityInfoBox.id = 'infoBox';
+        abilityInfoBox.appendChild(abilityInfo);
+        pokemonAbilityContainer.appendChild(abilityInfoBox);
+        pokemonAbilityContainer.appendChild(overlay);
+        for (let i = 0; i < abilities.length; i++) {
+            if (selectedPokemonAbilities.includes(abilities[i])) {
+                let abilityButton = document.createElement('button');
+                abilityInfo.innerText = data.find(pokemon => pokemon.name === selectedPokemon.value).abilities[i];
+                abilityButton.innerText = abilities[i];
+                abilityButton.id = 'abilityButton';
+                pokemonAbilityContainer.appendChild(abilityButton);
+                abilityButton.addEventListener('click', (e) => {
+                    overlay.style.display = 'block';
+                    abilityInfoBox.style.display = 'block';
+                    e.stopPropagation();
+                })
+                window.addEventListener('click', () => {
+                    overlay.style.display = 'none';
+                    abilityInfoBox.style.display = 'none';
+                });
+            }
         }
     }
-}
 
-function renderCard(data) {
-    resetCard();
-    let pokemonCard = document.querySelector('#pokemonCard');
-    let pokemonImage = document.createElement('img');
-    let pokemonName = document.createElement('h3');
-    pokemonImage.src = data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).image;
-    pokemonName.innerText = data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).name;
-    pokemonCard.appendChild(pokemonImage);
-    pokemonCard.appendChild(pokemonName);
-}
-
-function resetCard() {
-    const originalCard = document.querySelector('#pokemonCard');
-    while (originalCard.firstChild) {
-        originalCard.removeChild(originalCard.firstChild);
+    function renderWeakness(data) {
+        let selectedPokemon = document.querySelector('#pokemonSelect');
+        let pokemonWeaknessContainer = document.querySelector('#pokemonWeaknessButtons');
+        let weaknesses = ['Grass', 'Fire', 'Water', 'Electric', 'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'];
+        let selectedPokemonWeakness = data.find(pokemon => pokemon.name === selectedPokemon.value).weaknesses;
+        for (let i = 0; i < weaknesses.length; i++) {
+          if (selectedPokemonWeakness.includes(weaknesses[i])) {
+            const weaknessButton = document.createElement('button');
+            weaknessButton.innerText = weaknesses[i];
+            weaknessButton.classList.add(weaknesses[i]);
+            pokemonWeaknessContainer.appendChild(weaknessButton);
+          }
+        }
     }
-}
+
+    function renderDescription(data) {
+        let pokemonStats = document.querySelector('#pokemonStats');
+        let pokemonDescription = document.createElement('p');
+        pokemonDescription.id = 'pokemonDescription';
+        pokemonDescription.innerText = data.find(pokemon => pokemon.name === document.querySelector('#pokemonSelect').value).description;
+        pokemonStats.appendChild(pokemonDescription);
+    }
+
+    function resetStats() {
+        const originalStats = document.querySelector('#pokemonStats');
+        while (originalStats.firstChild) {
+            originalStats.removeChild(originalStats.firstChild);
+        }
+    }
+  
+    function resetCard() {
+        const originalCard = document.querySelector('#pokemonCard');
+        while (originalCard.firstChild) {
+            originalCard.removeChild(originalCard.firstChild);
+        }
+    }
+  })
