4826b9623eec544e5de8583d28c5618a389697d1
Brian Ashman <brian.ashman@comcast.net>
2023-05-08 22:08:07 -0700

    Fixes JavaScript issues with persisting content on page refresh; adds comments above functions detailing function purpose.

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-    e.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    getCollectedPokemon();
     fetch('http://localhost:3000/pokemon')
         .then(res => res.json())
         .then(data => {
             createPokemonSelection(data);
-            renderPokemonSelection(data);
+            renderSelectedPokemon(data);
             document.querySelector('#addButton').addEventListener('click', (e) => {
                 e.preventDefault();
-                addPokemon(data);
-            })
+                addToPokemonCollection(data);
+        })
     })
 
-    function createCollectionCard(pokemonObj) {
-        fetch('http://localhost:3000/collection', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(pokemonObj)
+    //=> Called on DOM intialization to get "collection" root info from index.json and render already collected cards to the collection interface on page load
+    function getCollectedPokemon() {
+        fetch('http://localhost:3000/collection')
+        .then(res => res.json())
+        .then(pokemonData => {
+            console.log(pokemonData);
+            initializePokemonCollection(pokemonData);
         })
     }
 
-    function renderPokemonSelection (data) {
-        let selectCard = document.querySelector('#searchButton')
-        selectCard.addEventListener('click', (e) => {
-            e.preventDefault();
-            renderCard(data);
-            renderStats(data);
-        })
+    //=> Iterates over the "collection" root in index.json and calls renderCollectedPokemon function for each pokemon
+    function initializePokemonCollection(pokemonData) {
+        pokemonData.forEach(pokemon => renderCollectedPokemon(pokemon));
     }
 
-    function addPokemon(data) {
+    //=> Whereas the addToPokemonCollection simply duplicates the originally searched for pokemon card and POSTs its json data to the "collection" root, this function re-renders the card on page refresh so cards are persisted on the page
+    function renderCollectedPokemon(pokemon) {
+        let collectionInterface = document.querySelector('#collectionInterface');
+        let pokemonCard = document.createElement('div');
+        let pokemonImage = document.createElement('img');
+        let pokemonName = document.createElement('h3');
+        let pokemonTypeContainer = document.createElement('div');
+        pokemonCard.id = 'collectedCard';
+        pokemonTypeContainer.id = 'pokemonType';
+        pokemonImage.src = pokemon.image;
+        pokemonName.innerText = pokemon.name;
+        let types = ['Grass', 'Fire', 'Water', 'Electric', 'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'];
+        for (let i = 0; i < types.length; i++) {
+            if (pokemon.type.includes(types[i])) {
+            const typeButton = document.createElement('button');
+            typeButton.innerText = types[i];
+            typeButton.classList.add(types[i]);
+            pokemonTypeContainer.appendChild(typeButton);
+            }
+        }
+        pokemonCard.appendChild(pokemonImage);
+        pokemonCard.appendChild(pokemonName);
+        pokemonCard.appendChild(pokemonTypeContainer);
+        collectionInterface.appendChild(pokemonCard);
+    }
+
+    
+    //=> Adds the currently selected pokemon's card to the collection interface, as well as adds it to the "collection" root within index.json;
+    function addToPokemonCollection(data) {
         let highestId = Math.max(...data.map(pokemon => pokemon.id));
         let interface = document.querySelector('#collectionInterface');
         let selectedPokemon = document.querySelector('#pokemonSelect').value;
@@ -43,11 +67,45 @@ document.addEventListener('DOMContentLoaded', (e) => {
                 let collectedCard = originalCard.cloneNode(true);
                 collectedCard.id = 'collectedCard';
                 interface.appendChild(collectedCard);
-                createCollectionCard(pokemonObj);
+                postCollectedPokemon(pokemonObj);
             }
         })
     }
-  
+
+    //=> Used in the above function to POST the pokemon added to the collection interface to the "collection" root within index.json; allows pokemon in the collection to persist on page refresh
+    function postCollectedPokemon(pokemonObj) {
+        fetch('http://localhost:3000/collection', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pokemonObj)
+        })
+    }
+
+    //=> Removes the pokemon from the pokemon collection interface, as well as removing the data from the "collection" root of index.json
+    function removeFromPokemonCollection(data){}
+
+    //=> Handles DELETE of pokemon from index.json
+    function deleteCollectedPokemon(pokemonObj){}
+
+    //=> Handles the details of the event of favoriting a pokemon in the collection interface
+    function favoritePokemon(data){}
+
+    //=> Handles PATCH of the favorite key/value pair in the "collection" root of index.json
+    function patchFavorite(pokemonObj){}
+    
+    //=> Renders the pokemon selected from the drop down menu on the pokedex interface when the search button is clicked
+    function renderSelectedPokemon (data) {
+        let selectCard = document.querySelector('#searchButton')
+        selectCard.addEventListener('click', (e) => {
+            e.preventDefault();
+            renderPokedexCard(data);
+            renderPokemonStats(data);
+        })
+    }
+
+    //=> Creates the initial pokemon selection options within the dropdown menu
     function createPokemonSelection(data) {
         data.forEach((pokemon) => {
             let pokemonSelection = document.querySelector('#pokemonSelect');
@@ -57,22 +115,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
         });
     };
 
-    function createTypeButtons(data) {
-        let selectedPokemon = document.querySelector('#pokemonSelect');
-        let pokemonTypeContainer = document.querySelector('#pokemonType');
-        let types = ['Grass', 'Fire', 'Water', 'Electric', 'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'];
-        let selectedPokemonType = data.find(pokemon => pokemon.name === selectedPokemon.value).type;
-        for (let i = 0; i < types.length; i++) {
-            if (selectedPokemonType.includes(types[i])) {
-            const typeButton = document.createElement('button');
-            typeButton.innerText = types[i];
-            typeButton.classList.add(types[i]);
-            pokemonTypeContainer.appendChild(typeButton);
-            }
-        }
-        }
-
-    function renderCard(data) {
+    //=> Renders the card displayed on the Pokedex Interface; includes the pokemon image, name, and pokemon types
+    function renderPokedexCard(data) {
         resetCard();
         resetStats();
         let pokemonCard = document.querySelector('#pokemonCard');
@@ -88,7 +132,24 @@ document.addEventListener('DOMContentLoaded', (e) => {
         createTypeButtons(data);
     }
 
-    function renderStats(data) {
+    //=> Creates the pokemon types displayed on the pokemon card; types created as buttons for potential implementation of additional pokemon data or sorting pokemon by type
+    function createTypeButtons(data) {
+        let selectedPokemon = document.querySelector('#pokemonSelect');
+        let pokemonTypeContainer = document.querySelector('#pokemonType');
+        let types = ['Grass', 'Fire', 'Water', 'Electric', 'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'];
+        let selectedPokemonType = data.find(pokemon => pokemon.name === selectedPokemon.value).type;
+        for (let i = 0; i < types.length; i++) {
+            if (selectedPokemonType.includes(types[i])) {
+            const typeButton = document.createElement('button');
+            typeButton.innerText = types[i];
+            typeButton.classList.add(types[i]);
+            pokemonTypeContainer.appendChild(typeButton);
+            }
+        }
+    }
+
+    //=> Renders the pokemon statistics seen on the pokedex interface; include the pokemon height, weight, category, weaknesses, and description
+    function renderPokemonStats(data) {
         let pokemonStats = document.querySelector('#pokemonStats');
         let pokemonHeight = document.createElement('h4');
         let pokemonWeight = document.createElement('h4');
@@ -111,12 +172,13 @@ document.addEventListener('DOMContentLoaded', (e) => {
         pokemonStats.appendChild(pokemonWeaknessButtons);
         pokemonStats.appendChild(pokemonAbility);
         pokemonStats.appendChild(pokemonAbilityButtons);
-        renderWeakness(data);
-        renderAbility(data);
-        renderDescription(data);
+        renderPokemonWeakness(data);
+        renderPokemonAbility(data);
+        renderPokemonDescription(data);
     }
 
-    function renderAbility(data) {
+    //=> W.I.P!! This functions purpose is to render the pokemon abilities seen within the statistics side of the pokemon interface; intended functionality not fully implemented
+    function renderPokemonAbility(data) {
         let selectedPokemon = document.querySelector('#pokemonSelect');
         let pokemonAbilityContainer = document.querySelector('#pokemonAbilities');
         let abilities = [...new Set(data.map(pokemon => pokemon.abilities).flat())];
@@ -149,7 +211,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }
     }
 
-    function renderWeakness(data) {
+    //=> Renders the Pokemon Weaknesses seen on the statistics side of the Pokedex Interface; weaknesses rendered as buttons for future implementation of displaying additional pokemon implementation or sorting pokemon by weakness
+    function renderPokemonWeakness(data) {
         let selectedPokemon = document.querySelector('#pokemonSelect');
         let pokemonWeaknessContainer = document.querySelector('#pokemonWeaknessButtons');
         let weaknesses = ['Grass', 'Fire', 'Water', 'Electric', 'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'];
@@ -164,7 +227,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }
     }
 
-    function renderDescription(data) {
+    //=> Renders the description of the pokemon seen on the statistics side of the Pokedex Interface
+    function renderPokemonDescription(data) {
         let pokemonStats = document.querySelector('#pokemonStats');
         let pokemonDescription = document.createElement('p');
         pokemonDescription.id = 'pokemonDescription';
@@ -172,6 +236,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
         pokemonStats.appendChild(pokemonDescription);
     }
 
+    //=> Resets Pokemon Statistics on the Pokedex Interface when a new Pokemon is searched
     function resetStats() {
         const originalStats = document.querySelector('#pokemonStats');
         while (originalStats.firstChild) {
@@ -179,6 +244,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }
     }
 
+    //=> Resets Pokemon Card on the Pokedex Interface when a new Pokemon is searched
     function resetCard() {
         const originalCard = document.querySelector('#pokemonCard');
         while (originalCard.firstChild) {
